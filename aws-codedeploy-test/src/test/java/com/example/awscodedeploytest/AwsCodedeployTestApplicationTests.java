dfe96c4f61441512fba168b05e57f652e19c953a
ohju96 <ohju96@naver.com>
2022-10-13 16:23:36 +0900

    setting

---
+++ aws-codedeploy-test/src/test/java/com/example/awscodedeploytest/AwsCodedeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscodedeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodedeployTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
